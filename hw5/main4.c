4d9694234ca898bf15c1dec353dd561f4a6387cd
B1329013 <b1329013@cgu.edu.tw>
2024-12-10 00:00:37 +0800

    Update main4.c

--- hw5/main4.c
+++ hw5/main4.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
-int main() {
-    int i = 12345
-    if (i < 10) {
-       printf("%d\n", i * 1000); 
-    }
-    else if (i < 100) {
-        int a = i % 10;
-        printf("%d\n", a * 1000 + i - a);
-    }
-    else if (i < 1000) {
-        int b = i % 10;
-        printf("%d\n", b * 1000 + i - b);
+int main () {
+    int i = 12345;
+    
+    if (i < 1000) {
+        printf("%d\n", 999 * (i % 10) + i);
     }
     else {
-        int c = i / 10000;
-        int d = i % 10000;
-        int e = d / 1000;
-        int f = d % 10;
-        int g = d % 1000;
-        printf("%d\n", c * 10000 + f * 1000 + g - f + e);
+        int a = (int) (i / 10000);
+        int b = (i / 1000) % 10;
+        int c = i % 10;
+        printf("%d", a * 10000 + c * 999 + i % 1000 + b);
     }
     return 0;
 }
